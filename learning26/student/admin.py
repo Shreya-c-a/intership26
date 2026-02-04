9a8a1bce2df0af99eeec92af5338f73c6b5dbc85
shreya-c-a <shahshreyan15@gmail.com>
2026-02-04 12:08:02 +0530

    table_created_4-2-26

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, Product,cart
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Product)
+admin.site.register(cart)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    studentName= models.CharField(max_length=100)
+    studentAge = models.IntegerField()
+    studentCity = models.CharField(max_length=40)
+    studentEmail = models.EmailField(null=True)
+
+    #meta class
+    class Meta:
+        db_table = "student" #table name
+
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productPrice = models.IntegerField()
+    productDescription = models.TextField()
+    productStock = models.PositiveIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+    class meta:
+        db_table = "product"
+
+class cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = "cart"
